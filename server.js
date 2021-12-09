31a92cdc2d3e2ea7cca388cf0e727578342e3a04
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-09 12:09:59 +0530

    updated index file

--- server.js
+++ server.js
@@ -37,13 +37,13 @@ app.use("/api/users", userRoute);
 app.use("/api/res", restaurantRoute);
 app.use("/api/order", orderRoute);
 
-//if (process.env.NODE_ENV === "production") {
-  //app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-  //app.get("*", (req, res) => {
-    //res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  //});
-//}
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "React", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
